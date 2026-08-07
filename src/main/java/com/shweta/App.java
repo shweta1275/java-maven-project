a1da718d7afe6d749de7768be9409d3cdb11768f
Shweta <shwetak1275@gmail.com>
2026-08-07 12:53:48 +0530

    Initial Maven project

---
+++ src/main/java/com/shweta/App.java
@@ -0,0 +1,13 @@
+package com.shweta;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
